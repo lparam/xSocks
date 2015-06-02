1baa30313fdf7115dddd07fc69a18d33ef2fe7c6
Ken Liao <lparam@live.com>
2015-06-02 21:58:03 +0800

    update logger

--- src/logger.c
+++ src/logger.c
@@ -5,27 +5,27 @@
 #include <string.h>
 #include <time.h>
 #include <assert.h>
-#ifdef _WIN32
-#include <io.h>
-#else
+#ifndef _WIN32
 #include <syslog.h>
 #endif
 
 #include "uv.h"
 #include "logger.h"
 
+
 #define LOG_MESSAGE_SIZE 256
 
 static int _syslog = 0;
+#ifdef _WIN32
 static uv_tty_t _tty;
 static uv_tty_t _ttyerr;
-static uv_loop_t _loop;
+#endif
 
-static char *levels[] = {
+static const char *levels[] = {
     "EMERG", "ALERT", "CRIT", "ERR", "WARNING", "NOTICE", "INFO", "DEBUG"
 };
 
-static char *colors[] = {
+static const char *colors[] = {
     "\033[01;31m", "\033[01;31m", "\033[01;31m", "\033[01;31m", "\033[01;33m", "\033[01;33m", "\033[01;32m", "\033[01;36m"
 };
 
@@ -34,37 +34,28 @@ static char *colors[] = {
 static void
 syslog(int priority, const char *format, ...) {
 }
-#endif
 
 static void
 tty_send_cb(uv_write_t *req, int status) {
-    printf("free req\n");
     free(req);
 }
 
 static void
 log2tty(uv_tty_t *tty, char *msg) {
-    uv_write_t req;
-
-    /* uv_buf_t buf;
-    buf.base = msg;
-    buf.len = strlen(msg); */
-
-	/* uv_tty_t tty;
-	uv_loop_t loop;
-	uv_loop_init(&loop);
-	uv_tty_init(&loop, &tty, 2, 0);
-	uv_tty_set_mode(&tty, UV_TTY_MODE_NORMAL); */
-
-    /* uv_write_t *req = malloc(sizeof(*req)); */
+    uv_write_t *req = malloc(sizeof(*req));
     uv_buf_t buf = uv_buf_init(msg, strlen(msg));
 
 	if (uv_guess_handle(1) == UV_TTY) {
-		uv_write(&req, (uv_stream_t*)tty, &buf, 1, tty_send_cb);
+		uv_write(req, (uv_stream_t*)tty, &buf, 1, tty_send_cb);
 	}
-
-	//fprintf(stderr, msg);
 }
+
+#else
+static void
+log2std(FILE *file, const char *msg) {
+    fprintf(file, "%s", msg);
+}
+#endif
 
 void
 logger_log(uint32_t level, const char *msg, ...) {
@@ -85,7 +76,12 @@ logger_log(uint32_t level, const char *msg, ...) {
         strftime(timestr, 20, "%Y/%m/%d %H:%M:%S", loctime);
         char m[300] = { 0 };
         sprintf(m, "%s%s [%s]\033[0m: %s\n", colors[level], timestr, levels[level], tmp);
+
+#ifdef _WIN32
         log2tty(&_tty, m);
+#else
+        log2std(stdout, m);
+#endif
     }
 }
 
@@ -105,42 +101,31 @@ logger_stderr(const char *msg, ...) {
     strftime(timestr, 20, "%Y/%m/%d %H:%M:%S", loctime);
     char m[300] = { 0 };
     sprintf(m, "\033[01;31m%s [%s]\033[0m: %s\n", timestr, levels[LOG_ERR], tmp);
+
+#ifdef _WIN32
     log2tty(&_ttyerr, m);
+#else
+    log2std(stderr, m);
+#endif
 }
 
 int
 logger_init(int syslog) {
-    int ttyfd;
-    int ttyerrfd;
-
-#ifdef _WIN32
-   /* HANDLE handle;
-
-	handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	assert(handle != INVALID_HANDLE_VALUE);
-	ttyfd = _open_osfhandle((intptr_t)handle, 0);
-
-	handle = GetStdHandle(STD_ERROR_HANDLE);
-	assert(handle != INVALID_HANDLE_VALUE);
-	ttyerrfd = _open_osfhandle((intptr_t)handle, 0);*/
-#else
-    ttyfd = 1;
-    ttyerrfd = 2;
+#ifndef _WIN32
     _syslog = syslog;
-#endif
-
-    uv_loop_init(&_loop);
-    uv_tty_init(&_loop, &_tty, 1, 0);
-    uv_tty_init(&_loop, &_ttyerr, 2, 0);
+#else
+    uv_tty_init(uv_default_loop(), &_tty, 1, 0);
+    uv_tty_init(uv_default_loop(), &_ttyerr, 2, 0);
     uv_tty_set_mode(&_tty, UV_TTY_MODE_NORMAL);
     uv_tty_set_mode(&_ttyerr, UV_TTY_MODE_NORMAL);
+#endif
 
     return 0;
 }
 
 void
 logger_exit() {
-    printf("logger exit\n");
-    uv_loop_close(&_loop);
+#ifdef _WIN32
     uv_tty_reset_mode();
+#endif
 }
